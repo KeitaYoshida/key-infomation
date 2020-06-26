f181b9aa661fc48fde49eeecd07a8c347cd56545
keita <ba0503naky@gmail.com>
2020-06-26 10:13:48 +0900

    change meta info

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
   siteMetadata: {
     title: `key infomation`,
     author: {
-      name: `keita yoshida`,
+      name: `keita`,
       summary: `who lives and works in San Francisco building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
